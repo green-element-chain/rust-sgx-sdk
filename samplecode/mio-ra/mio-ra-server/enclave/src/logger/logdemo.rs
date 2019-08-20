942d3fd26d4f88dedbac82003e68ad458209d368
webb.shi <bradyjoestar@gmail.com>
2019-08-20 05:05:44 -0400

    log to stdout

--- samplecode/mio-ra/mio-ra-server/enclave/src/logger/logdemo.rs
+++ samplecode/mio-ra/mio-ra-server/enclave/src/logger/logdemo.rs
@@ -1,6 +1,10 @@
+use env_logger::{Builder, Target};
+
 pub fn log_demo() {
     println!("------------------------------------------");
-    env_logger_gel::init();
+    let mut builder = Builder::from_default_env();
+    builder.target(Target::Stdout);
+    builder.init();
     info!("{}", "log test");
     trace!("{}", "log trace test");
     println!("------------------------------------------");
